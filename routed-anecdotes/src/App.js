9694b9ac91452a4eb6cbe5b6f83482d118443c55
Tom Radford <tom@theradford.com>
2022-07-11 13:23:58 +0200

    Exercises up to 7.8

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import {
   Link, Route, Routes, useMatch, useNavigate
 } from 'react-router-dom'
-
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -51,25 +51,31 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const [content, resetContent] = useField('text')
+  const [author, resetAuthor ] = useField('text')
+  const [info, resetInfo ] = useField('text')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
-    props.setNotification(`A new anecdote ${content} created!`)
+    props.setNotification(`A new anecdote ${content.value} created!`)
     setTimeout(() => {
       props.setNotification('')
     }, 5000)
+  }
+  const handleReset = (e) => {
+    e.preventDefault()
+    resetInfo()
+    resetContent()
+    resetAuthor()
   }
 
   return (
@@ -78,17 +84,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content}/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info}/>
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
